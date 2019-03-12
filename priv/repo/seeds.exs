e4725f1e72e7efdb19bc37acdc987b05c5e5f284
Davi Wesley <davi.wesley@icloud.com>
2019-03-11 22:44:57 -0300

    Update seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -15,5 +15,16 @@ user =
     email: "davi.wesley@icloud.com",
     password: "issodeveriaserumsegredo"
   })
-
+  
+firt_pet = Apiexample.Aninals.Pet.registracion_changeset(%Apiexample.Animals.Pet{}, %{
+ race: "Bombay",
+ average_life_time: 20,
+ average_height: 8,
+ allergies: "sinus, gingivitis",
+ nacionality: "USA and Thailand",
+ hair_color: "black",
+ eye_color: "copper or green",
+ })
+ 
 Apiexample.Repo.insert!(user)
+Apiexample.Repo.insert!(first_pet)
