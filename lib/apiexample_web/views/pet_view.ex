516d47b922e26d837170858b9e3a0d35e048c9a0
daviwesley <davi.wesley@icloud.com>
2019-03-21 17:37:30 -0300

    refactor PetView

--- lib/apiexample_web/views/pet_view.ex
+++ lib/apiexample_web/views/pet_view.ex
@@ -2,7 +2,8 @@ defmodule ApiexampleWeb.PetView do
   use ApiexampleWeb, :view
   use JaSerializer.PhoenixView
 
-  attributes [:race, :nacionality, :image, :average_life_time, :average_height, :eye_color,
-              :hair_color, :allergies
-]
+  attributes [:race, :nacionality, :image,
+              :average_life_time, :average_height,
+              :eye_color, :hair_color, :allergies
+             ]
 end
