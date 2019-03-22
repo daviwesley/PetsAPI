6632502ea4fe5d5153b3ecd88407739a1152d574
daviwesley <davi.wesley@icloud.com>
2019-03-21 22:50:23 -0300

    add extra line(EOF)

--- lib/apiexample/auth/error_handler.ex
+++ lib/apiexample/auth/error_handler.ex
@@ -1,20 +1,20 @@
 defmodule Apiexample.Auth.ErrorHandler do
     import Plug.Conn
-  
+
     def auth_error(conn, {:invalid_token, _reason}, _opts),
       do: response(conn, :unauthorized, "Invalid Token")
-  
+
     def auth_error(conn, {:unauthenticated, _reason}, _opts),
-      do: response(conn, :unauthorized, "Not Authenticated")
-  
+      do: response(conn, :unauthorized, "Did you provide a token? ex Authorization: Bearer yourtoken")
+
     def auth_error(conn, {:no_resource_found, _reason}, _opts),
       do: response(conn, :unauthorized, "No Resource Found")
-  
+
     def auth_error(conn, {type, _reason}, _opts), do: response(conn, :forbidden, to_string(type))
-  
+
     defp response(conn, status, message) do
       body = Poison.encode!(%{error: message})
-  
+
       conn
       |> put_resp_content_type("application/json")
       |> send_resp(status, body)
